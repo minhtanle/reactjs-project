edbf2acb7e5d00aff771c3d35568881dc2bbe44f
Minh Tân Lê <tan.lm@fibo.vn>
2024-01-17 23:43:57 +0700

    AuthGuard : keep path when reload page

--- src/guards/AuthGuard.jsx
+++ src/guards/AuthGuard.jsx
@@ -1,15 +1,26 @@
-import useAuth from "../hooks/useAuth"
-import Loading from "@/components/Loading"
+import { useState, useEffect } from 'react';
+import useAuth from "../hooks/useAuth";
+import Loading from "@/components/Loading";
 import { Navigate } from "react-router-dom";
 
 const AuthGuard = ({ children }) => {
-    const { isAuthenticated, isLoading } = useAuth()
+    const { isAuthenticated, isLoading } = useAuth();
+    const [redirectPath, setRedirectPath] = useState(null);
+
+    useEffect(() => {
+        if (!isAuthenticated && !redirectPath) {
+            // Store the current path for redirection after authentication
+            setRedirectPath(window.location.pathname);
+        }
+    }, [isAuthenticated, redirectPath]);
 
     if (isLoading) return <Loading />;
 
-    if (!isAuthenticated) return <Navigate to="/login" replace />;
+    if (!isAuthenticated && redirectPath) {
+        return <Navigate to="/login" replace />;
+    }
 
-    return (<>{children}</>)
+    return <>{children}</>;
 }
 
-export default AuthGuard
+export default AuthGuard;
